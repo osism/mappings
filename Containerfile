d341d760a7fa3ce4fe0af5e543eff8ffc7bbaf41
Christian Berendt <berendt@osism.tech>
2022-01-20 21:38:35 +0100

    Initial commit
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ Containerfile
@@ -0,0 +1,24 @@
+ARG PYTHON_VERSION=3.9
+
+ARG CEPH_ANSIBLE_VERSION=pacific
+ARG KOLLA_ANSIBLE_VERSION=wallaby
+ARG OSISM_ANSIBLE_VERSION=latest
+
+FROM quay.io/osism/ceph-ansible:${CEPH_ANSIBLE_VERSION} as ceph-ansible
+FROM quay.io/osism/kolla-ansible:${KOLLA_ANSIBLE_VERSION} as kolla-ansible
+FROM quay.io/osism/osism-ansible:${OSISM_ANSIBLE_VERSION} as osism-ansible
+
+
+FROM python:${PYTHON_VERSION} as builder
+
+COPY --from=ceph-ansible /ansible /ceph-ansible
+COPY --from=kolla-ansible /ansible /kolla-ansible
+COPY --from=osism-ansible /ansible /osism-ansible
+COPY --from=osism-ansible /usr/share/ansible/collections /usr/share/ansible/collections
+COPY --from=osism-ansible /usr/share/ansible/roles /usr/share/ansible/roles
+
+COPY . /src
+WORKDIR /src
+
+RUN python3 -m pip --no-cache-dir install -U 'pip==21.3.1' 'pipenv==2022.1.8' \
+    && pipenv install
---
+++ generate.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+
+import glob
+import logging
+import os
+import re
+
+from ruamel.yaml import YAML
+
+yaml = YAML()
+
+level = logging.INFO
+logging.basicConfig(format='%(asctime)s - %(message)s', level=level, datefmt='%Y-%m-%d %H:%M:%S')
+
+ANSIBLE_DIRECTORIES = [
+    '/ceph-ansible',
+    '/kolla-ansible',
+    '/osism-ansible'
+]
+
+mapping1 = {}
+mapping2 = {}
+
+for ansible_directory in ANSIBLE_DIRECTORIES:
+    logging.info(f"Analyzing {ansible_directory}")
+    for p in [x[0] for x in os.walk(ansible_directory, followlinks=True)]:
+        b = os.path.basename(p)
+        if b in ["defaults"]:
+            # skip integration tests of the ansible community collections
+            if "tests/integration" in p:
+                continue
+
+            logging.info(f"Found {b} in {p}")
+
+            for f in [x for x in glob.iglob(p + '**/**', recursive=True) if os.path.isfile(x)]:
+
+                # skip integration tests of the ansible community collections
+                if "tests/integration" in f:
+                    continue
+
+                if "collections/ansible_collections" in f:
+                    pattern = ".*/collections/ansible_collections/(.*)/(.*)/roles/(.*)/defaults"
+                    match = re.search(pattern, f)
+                    rolename = f"{match.group(1)}.{match.group(2)}.{match.group(3)}"
+                elif "galaxy/roles/" in f:
+                    pattern = ".*/galaxy/roles/(.*)/defaults"
+                    match = re.search(pattern, f)
+                    rolename = f"{match.group(1)}"
+                elif "galaxy/" in f:
+                    pattern = "galaxy/(.*)/defaults"
+                    match = re.search(pattern, f)
+                    rolename = f"{match.group(1)}"
+                else:
+                    pattern = "roles/(.*)/defaults"
+                    match = re.search(pattern, f)
+
+                    if "ceph" in ansible_directory:
+                        rolename = f"ceph.{match.group(1)}"
+                    elif "kolla" in ansible_directory:
+                        rolename = f"kolla.{match.group(1)}"
+                    else:
+                        rolename = f"{match.group(1)}"
+
+                if rolename in mapping1:
+                    continue
+
+                logging.info(f"Analyzing {rolename}")
+
+                mapping1[rolename] = []
+
+                with open(f, 'r') as fp:
+                    d = yaml.load(fp)
+
+                try:
+                    for parameter in d.keys():
+                        logging.info(f"Found parameter {parameter} in {rolename}")
+                        mapping1[rolename].append(parameter)
+
+                        if parameter not in mapping2:
+                            mapping2[parameter] = []
+                        mapping2[parameter].append(rolename)
+
+                except:  # noqa
+                    pass
+
+with open("mapping1.yml", "w+") as fp:
+    yaml.dump(mapping1, fp)
+
+with open("mapping2.yml", "w+") as fp:
+    yaml.dump(mapping2, fp)
