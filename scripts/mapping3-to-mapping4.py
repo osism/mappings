a5a14472f91f13ec2d55325747260c9c2a30930b
Christian Berendt <berendt@osism.tech>
2022-06-13 14:07:33 +0200

    Fix trailing whitespace (#38)
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- scripts/mapping3-to-mapping4.py
+++ scripts/mapping3-to-mapping4.py
@@ -7,7 +7,7 @@ mapping4 = {}
 
 for k in data:
     mapping4.setdefault(data[k], [])
-    mapping4[data[k]].append(k) 
+    mapping4[data[k]].append(k)
 
 with open("mapping4.yml", "w+") as fp:
     fp.write(yaml.dump(mapping4))
